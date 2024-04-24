d63cfdc39f2d876672e5d28f753d6001dccdada5
Santiago Baldassin <santiago.baldassin@rdx.works>
2024-04-23 21:20:51 -0300

    removing not needed types

--- packages/xchain-radix/src/types/radix.ts
+++ packages/xchain-radix/src/types/radix.ts
@@ -1,31 +1,3 @@
-import { BaseAmount } from '@xchainjs/xchain-util/lib'
-
-/**
- * Parameters for `/stream/transactions` endpoint
- * @see https://radix-babylon-gateway-api.redoc.ly/#operation/StreamTransactions
- */
-export type GetTxsParams = {
-  atLedgerState?: number
-  fromLedgerState?: number
-  cursor?: number
-  limitPerPage?: number
-  affectedGlobalEntitiesFilter?: []
-}
-
-type TransactionData = {
-  transaction_status: string
-  state_version: number
-  epoch: number
-  round: number
-  round_timestamp: string
-  payload_hash: string
-  intent_hash: string
-  fee_paid: string
-  confirmed_at: string
-  receipt: any
-  manifest_classes: any
-}
-
 type LedgerState = {
   network: string
   state_version: number
@@ -45,20 +17,6 @@ type EntityDetails = {
 export type EntityDetailsResponse = {
   ledger_state: LedgerState
   items: EntityDetails[]
-}
-
-export type RadixTxResponse = {
-  ledger_state: LedgerState
-  transaction: TransactionData
-}
-
-export type RadixBalance = {
-  asset: RadixAsset
-  amount: BaseAmount
-}
-
-export type RadixAsset = {
-  resource_address: string
 }
 
 export type Transaction = {
